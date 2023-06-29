e7361d736a11426d61e92361faca776e7a28fd10
dimsth <dimo35xf@gmail.com>
2023-06-29 16:07:27 +0300

    [Add] New server configs

--- src/quic_server.c
+++ src/quic_server.c
@@ -9,5 +9,21 @@ int main(int argc, char **argv) {
   ngtcp2_settings *sett;
   ngtcp2_settings_default(sett);
 
+  ngtcp2_conn *conn;
+  ngtcp2_cid dcid;
+  ngtcp2_cid scid;
+
+  int dest_id = 1;
+  int sour_id = 2;
+
+  dcid.datalen = sizeof(dest_id);
+  memcpy(dcid.data, &dest_id, dcid.datalen);
+
+  scid.datalen = sizeof(sour_id);
+  memcpy(scid.data, &sour_id, scid.datalen);
+
+  int ret = ngtcp2_conn_server_new(&conn, &dcid, &scid, NULL, 0, NULL, sett,
+                                   NULL, NULL, NULL);
+
   return 0;
 }
