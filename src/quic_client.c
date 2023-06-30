678a68d393bd3b2bbb18cecda346c8eedfb4e9b6
dimsth <dimo35xf@gmail.com>
2023-06-30 14:57:52 +0300

    [Add] Socket for client

--- src/quic_client.c
+++ src/quic_client.c
@@ -1,8 +1,23 @@
+#include <netinet/in.h>
 #include <ngtcp2/ngtcp2.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
 
 int main(int argc, char **argv) {
   printf("Client");
+
+  int sockfd = socket(AF_LOCAL, SOCK_DGRAM, 0);
+  if (sockfd < 0) {
+    perror("socket");
+    exit(EXIT_FAILURE);
+  }
+
+  /* Set up server address */
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_LOCAL;
+  server_addr.sin_port = htons(12345);
 
   return 0;
 }
