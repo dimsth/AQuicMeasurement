33d6188a35c9e47d6cdddc2923d67afe2c839caa
dimsth <dimo35xf@gmail.com>
2023-06-30 16:14:38 +0300

    [Add] Client to get server ip from args and convert to binary

--- src/quic_client.c
+++ src/quic_client.c
@@ -1,3 +1,5 @@
+#include <arpa/inet.h>
+#include <assert.h>
 #include <netinet/in.h>
 #include <ngtcp2/ngtcp2.h>
 #include <stdio.h>
@@ -7,7 +9,12 @@
 #define PORT 8800
 
 int main(int argc, char **argv) {
+
+  assert(argc == 1);
+
   printf("Client");
+
+  printf("Server to connect: %s", argv[1]);
 
   int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
   if (sockfd < 0) {
@@ -19,6 +26,12 @@ int main(int argc, char **argv) {
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_port = htons(PORT);
+
+  /* Convert IPv4 and IPv6 addresses from text to binary form */
+  if (inet_pton(AF_INET, argv[1], &server_addr.sin_addr) <= 0) {
+    printf("\nInvalid address/ Address not supported \n");
+    return -1;
+  }
 
   return 0;
 }
