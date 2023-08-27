32792ba55d799bc387e4ad0dd400e525fafbbecb
dimsth <dimo35xf@gmail.com>
2023-08-27 16:52:01 +0300

    [Add] Gitignore folders that save measurements

--- scripts/measure_cpu.sh
+++ scripts/measure_cpu.sh
@@ -5,7 +5,7 @@ if [ $# -lt 3 ]; then
   echo "Usage: $0 <program_command> <client/server> <(ethtool) on/off>"
     exit 1
 fi
-output_folder="../measurments/$2"
+output_folder="../measurments/meas_$2"
 mkdir "$output_folder"
 
 program_command="$1"
