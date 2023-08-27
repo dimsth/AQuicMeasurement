acb9218ad54a2b220d0376e24684bdb6aa8e2f24
dimsth <dimo35xf@gmail.com>
2023-08-27 16:43:35 +0300

    [Add][Script] Add if condition to check that when ethtool is used arg 4 is set

--- scripts/measure_cpu.sh
+++ scripts/measure_cpu.sh
@@ -13,8 +13,10 @@ file_path_util="$output_folder/cpu_util_log_$2.txt"
 e_tool_before="$output_folder/ethtool_before_$2.txt"
 e_tool_after="$output_folder/ethtool_after_$2.txt"
 
-if [ "$3" = "on" ]; then
+if [ "$3" = "on" ] && [ -n "$4" ]; then
   ethtool -S $4 > "$e_tool_before"
+else
+  echo "Ethtool is set off or not interface was set"
 fi
 
 # Run the provided program command in the background
